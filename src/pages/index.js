ba0a6451d7be1f70566e67d6031123afb99b5e51
Kaleb Kohn <90642888+KohnKaleb@users.noreply.github.com>
2023-05-09 00:12:58 -0500

    react

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-// Step 1: Import React
+
 import * as React from 'react'
 import NavBar from './navbar.js'
-// Step 2: Define your component
+
 const IndexPage = () => {
   return (
     <main>
@@ -12,7 +12,6 @@ const IndexPage = () => {
   )
 }
 
-// You'll learn about this in the next task, just copy it for now
 export const Head = () => <title>Kaleb's Website</title>
 
 // Step 3: Export your component
--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -3,8 +3,10 @@ import * as React from 'react'
 class NavBar extends React.Component {
     render() {
         return <div>
-                    <a href="https://www.linkedin.com/in/kaleb-kohn-1b1b3a1b1/"><img src="../src/images/linkedin_icon.png" alt="linkedin icon"></img></a>
-                    <a href="https://github.com/KohnKaleb"><img src="../src/images/github_icon.png" alt="github icon"></img></a>
+                    <a href="https://www.linkedin.com/in/kaleb-kohn-2a1b3622b/"><img src="..\images\linkedin_icon.png" alt="linkedin icon"></img></a>
+                    <a href="https://github.com/KohnKaleb"><img src="../images/github_icon.png" alt="github icon"></img></a>
                 </div>
     }
 }
+
+export default NavBar
