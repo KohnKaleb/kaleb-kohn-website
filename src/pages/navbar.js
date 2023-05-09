068a5f9f5585620814869e8072d30b74c07b079c
Kaleb Kohn <90642888+KohnKaleb@users.noreply.github.com>
2023-05-09 00:01:07 -0500

    Trying to learn some React

---
+++ src/pages/navbar.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+class NavBar extends React.Component {
+    render() {
+        return <div>
+                    <a href="https://www.linkedin.com/in/kaleb-kohn-1b1b3a1b1/"><img src="../src/images/linkedin_icon.png" alt="linkedin icon"></img></a>
+                    <a href="https://github.com/KohnKaleb"><img src="../src/images/github_icon.png" alt="github icon"></img></a>
+                </div>
+    }
+}
