a3226a8a199d63c38f22ffc55bc0314e89d3e540
Kaleb Kohn <90642888+KohnKaleb@users.noreply.github.com>
2023-05-23 22:02:55 -0500

    we back! some minor changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,25 @@
-
-import * as React from 'react'
-import NavBar from './navbar.js'
+import * as React from "react";
+import NavBar from "./navbar.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const IndexPage = () => {
   return (
     <main>
       <h1>Kaleb Kohn</h1>
-      <NavBar/>
-      <p>Computer Science Major UW-Madison</p>
+      <NavBar />
+
+      <h1>About</h1>
+      <p>
+        Computer Science Major UW-Madison Passionate about computer science and
+        mostly interested in backend development but I like to try and learn all
+        aspects of software development I am also a member of the Air Force
+        National Guard and have been in for over 4 years now, and have gone on
+        one deployment to Saudi Arabia
+      </p>
     </main>
-  )
-}
+  );
+};
 
-export const Head = () => <title>Kaleb's Website</title>
+export const Head = () => <title>Kaleb's Website</title>;
 
-export default IndexPage
+export default IndexPage;
--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -1,12 +1,18 @@
-import * as React from 'react'
+import * as React from "react";
 
 class NavBar extends React.Component {
-    render() {
-        return <div>
-                    <a href="https://www.linkedin.com/in/kaleb-kohn-2a1b3622b/"><img src="..\images\linkedin_icon.png" alt="linkedin icon"></img></a>
-                    <a href="https://github.com/KohnKaleb"><img src="../images/github_icon.png" alt="github icon"></img></a>
-                </div>
-    }
+  render() {
+    return (
+      <div>
+        <a href="https://www.linkedin.com/in/kaleb-kohn-2a1b3622b/">
+          <img src="..\images\linkedin_icon.png" alt="linkedin icon"></img>
+        </a>
+        <a href="https://github.com/KohnKaleb">
+          <img src="../images/github_icon.png" alt="github icon"></img>
+        </a>
+      </div>
+    );
+  }
 }
 
-export default NavBar
+export default NavBar;
