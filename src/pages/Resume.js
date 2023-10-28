746ff8ff7f747acc6dc13ddd633ec1f4d2815295
kalebkohn <kjkohn@wisc.edu>
2023-10-28 00:19:45 -0500

    added resume!

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Layout from "../components/layout";
+import kaleb_resume from "../static/kaleb_resume.jpg"
+import "../css/resume.css"
 
 const Resume = () => {
   return (
     <Layout>
-      <h1>Resume</h1>
+      <div className="image-scroll-container">
+        <div className="image-wrapper">
+          <img src={kaleb_resume} alt="Kaleb's Resume" />
+        </div>
+      </div>
     </Layout>
   );
 };
