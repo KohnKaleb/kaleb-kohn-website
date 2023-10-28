4d1abc039821a1f8001cb01ef64b823c818ae2a0
kalebkohn <kjkohn@wisc.edu>
2023-10-27 23:50:00 -0500

    navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,15 @@ import "../css/navbar.css";
 
 export default function NavBar() {
   return (
-    <nav className="navbar">
-      <Link to="/" className="nav-link">Home</Link>
-      <Link to="/aboutMe" className="nav-link">About</Link>
-      <Link to="/resume" className="nav-link">Resume</Link>
-      <Link to="/contact" className="nav-link">Contact</Link>
-    </nav>
+    <>
+      <div className="header">
+        <div className="main-nav">
+          <Link to="/">Home</Link>
+          <Link to="/aboutMe">About</Link>
+          <Link to="/resume">Resume</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+      </div>
+    </>
   );
 }
--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -1,14 +1,17 @@
 import * as React from "react";
 import { Link } from "gatsby";
+import "../css/navbar.css";
 
 export default function NavBar() {
   return (
     <>
-      <div className="navbar menu-items">
-        <Link to="/">Home</Link>
-        <Link to="/aboutMe">About</Link>
-        <Link to="/resume">Resume</Link>
-        <Link to="/contact">Contact</Link>
+      <div className="header">
+        <div className="main-nav">
+          <Link to="/">Home</Link>
+          <Link to="/aboutMe">About</Link>
+          <Link to="/resume">Resume</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
       </div>
     </>
   );
